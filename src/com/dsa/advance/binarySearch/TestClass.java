c578308a037ff56745ccc21451f1d4c2754d6f70
Khagesh257484 <48479126+Khagesh257484@users.noreply.github.com>
2022-08-18 12:34:41 +0530

    Adding new Programm

---
+++ src/com/dsa/advance/binarySearch/TestClass.java
@@ -0,0 +1,9 @@
+package com.dsa.advance.binarySearch;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Test class for github to check ...........");
+        System.out.println("Hi Git baba........");
+    }
+
+}
