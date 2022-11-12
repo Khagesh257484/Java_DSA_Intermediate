e3b212aa186f1e5dcf3ab3ad2f884852f80c9454
Khagesh257484 <48479126+Khagesh257484@users.noreply.github.com>
2022-11-12 08:57:40 +0530

    Adding new Programm

---
+++ src/com/dsa/advance/binaryTree/TreeNode.java
@@ -0,0 +1,13 @@
+package com.dsa.advance.binaryTree;
+
+// This class will help us to create the node of the tree, and you can traversal on this class.
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
