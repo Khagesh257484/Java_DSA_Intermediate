2dcd740e2ed651d9c7023047474247d052534811
khagesh.sharma <rk92khagesh92@gmail.com>
2023-03-09 08:19:24 +0530

    Test Commit from Office Lapi

---
+++ OOPS/src/constructor/Test.java
@@ -0,0 +1,11 @@
+package constructor;
+
+public class Test {
+    public Test() {
+        System.out.printf("Default constructor");
+    }
+
+    public static void main(String[] args) {
+        Test t= new Test();
+    }
+}
